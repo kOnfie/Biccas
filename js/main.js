79eab3488f9ae7ea7f98f5727f59e3d6af99b326
denismatvieiev <matveevdenis458@gmail.com>
2025-07-01 15:28:33 +0300

    feat(elips): add elips for design feat(hero): add hero section to page feat(collabs): add collabs section to page feat(support): add support section to page

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ const headerBurgerButton = document.querySelector(".header__burger");
 headerBurgerButton.addEventListener("click", function () {
   headerBurgerButton.classList.toggle("active");
   document.querySelector(".header__nav").classList.toggle("active");
+
+  document.body.style.overflow = "hidden";
+
+  if (!headerBurgerButton.classList.contains("active")) {
+    document.body.style.overflow = "auto";
+  }
 });
