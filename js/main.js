d6b69bbcf33cfeec7877e0181fc2ea9992124182
denismatvieiev <matveevdenis458@gmail.com>
2025-07-02 12:00:16 +0300

    feat(choose): add choose section

--- js/main.js
+++ js/main.js
@@ -10,3 +10,41 @@ headerBurgerButton.addEventListener("click", function () {
     document.body.style.overflow = "auto";
   }
 });
+
+// choose plan
+const chooseMonthlyBtn = document.querySelector("#monthly-choose-plan");
+const chooseYearlyBtn = document.querySelector("#yearly-choose-plan");
+
+const priceBusinessDisplay = document.querySelector("#price--business--plan");
+const priceProDisplay = document.querySelector("#price--pro--plan");
+
+function updatePlan(plan) {
+  if (plan === "monthly") {
+    chooseMonthlyBtn.classList.add("active");
+    chooseYearlyBtn.classList.remove("active");
+
+    priceProDisplay.innerHTML = `<span>$</span> 8`;
+    priceBusinessDisplay.innerHTML = `<span>$</span> 16`;
+  } else if (plan === "yearly") {
+    chooseYearlyBtn.classList.add("active");
+    chooseMonthlyBtn.classList.remove("active");
+
+    priceProDisplay.innerHTML = `<span>$</span> 48`;
+    priceBusinessDisplay.innerHTML = `<span>$</span> 96`;
+  }
+}
+
+const savedPlan = localStorage.getItem("plan");
+if (savedPlan) {
+  updatePlan(savedPlan);
+}
+
+chooseMonthlyBtn.addEventListener("click", () => {
+  updatePlan("monthly");
+  localStorage.setItem("plan", "monthly");
+});
+
+chooseYearlyBtn.addEventListener("click", () => {
+  updatePlan("yearly");
+  localStorage.setItem("plan", "yearly");
+});
